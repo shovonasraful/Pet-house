15dace6f517de8450d1df698b59dbc6483d05359
shovonasraful <ahmedshovon279@gmail.com>
2025-02-01 10:28:58 +0000

    add card

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "rgb(255, 220, 38)",
+          
+          
+        },
+      },
+    },
+    plugins: [],
+  }
